c5a9fe9fa799d932bab4bfb264fba393ee76ebe0
Bhavesh Mankar <bmmankar25@gmail.com>
2023-03-28 11:33:07 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        Coded with ❤️ by{" "}
+        <a
+          className="footer--link"
+          href="https://www.linkedin.com/in/bhavesh-mankar-7420ba22a"
+          target="_blank"
+          rel="noreferrer"
+        >
+           Bhavesh Mankar
+        </a>
+        .
+      </small>
+    </footer>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,37 @@
+const Data = [
+  {
+    key: 1,
+    title: "Statue of Unity",
+    location: "Gujarat, India",
+    googleMapsUrl: "https://goo.gl/maps/ZJTMpEaDbvPK9jkr6",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Statue of Unity is a colossal statue of Indian independence activist Sardar Vallabhbhai Patel, located in Gujarat, India. The statue is located on the Sadhu Bet Island, near the Sardar Sarovar Dam, on the Narmada River, in the state of Gujarat.",
+    imageUrl: "https://images.unsplash.com/photo-1642841819300-20ed449c02a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+  },
+  {
+    key: 2,
+    title: "The Taj Mahal",
+    location: "Agra, Uttar Pradesh, India",
+    googleMapsUrl: "https://goo.gl/maps/r3Q37txTAgSr1TCY9",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
+    description:
+      "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the Yamuna river in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor, Shah Jahan (reigned from 1628 to 1658), to house the tomb of his favourite wife, Mumtaz Mahal.",
+    imageUrl: "https://images.unsplash.com/photo-1564507592333-c60657eea523?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+  },
+  {
+    key: 3,
+    title: "Ajanta Caves",
+    location: "Ajanta, Maharashtra, India",
+    googleMapsUrl: "https://www.google.com/maps/place/Ajanta+Caves/@20.5518603,75.7010634,17z/data=!3m1!4b1!4m6!3m5!1s0x3bd97f7014a75e43:0x7ca8d7c57639691f!8m2!3d20.5518603!4d75.7032521!16zL20vMF96Nw",
+    startDate: "01 Oct, 2021",
+    endDate: "18 Nov, 2021",
+    description:
+      "Ajaṇṭā is a Buddhist rock-cut cave site in Aurangabad District of Maharashtra State, western India. It was discovered in 1819 by British Army officers who were hunting tigers in the great gorge of the Waghora River. The caves are numbered thirty in total and are divided into two phases",
+    imageUrl: "https://images.unsplash.com/photo-1620558601903-9f2441730121?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+  },
+];
+
+export default Data;
